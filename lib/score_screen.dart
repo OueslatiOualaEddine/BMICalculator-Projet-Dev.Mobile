b07880a4fc3671aabb78d4c611d452ed25477844
Oueslati Ouala Eddine <oualaeddine.oueslati@esen.tn>
2024-08-25 14:06:14 +0100

    BMICalculator

---
+++ lib/score_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:pretty_gauge/pretty_gauge.dart';
+import 'package:share_plus/share_plus.dart';
+
+class ScoreScreen extends StatelessWidget {
+  final double bmiScore;
+
+  final int age;
+
+  String? bmiStatus;
+
+  String? bmiInterpretation;
+
+  Color? bmiStatusColor;
+
+  ScoreScreen({Key? key, required this.bmiScore, required this.age})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    setBmiInterpretation();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+          foregroundColor : Colors.white,
+        title: const Text("BMI Results", style: TextStyle(
+            color: Colors.white
+        )),
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(12),
+          child: Card(
+              elevation: 12,
+              shape: const RoundedRectangleBorder(),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Your Score",
+                      style: TextStyle(fontSize: 30, color: Colors.blue),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    PrettyGauge(
+                      gaugeSize: 275,
+                      minValue: 0,
+                      maxValue: 40,
+                      segments: [
+                        GaugeSegment('UnderWeight', 18.5, Colors.red),
+                        GaugeSegment('Normal', 6.4, Colors.green),
+                        GaugeSegment('OverWeight', 5, Colors.orange),
+                        GaugeSegment('Obese', 10.1, Colors.pink),
+                      ],
+                      valueWidget: Text(
+                        bmiScore.toStringAsFixed(1),
+                        style: const TextStyle(fontSize: 40),
+                      ),
+                      currentValue: bmiScore.toDouble(),
+                      needleColor: Colors.blue,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      bmiStatus!,
+                      style: TextStyle(fontSize: 30, color: bmiStatusColor!),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      bmiInterpretation!,
+                      style: const TextStyle(fontSize: 25),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text(
+                                "Re-calculate",
+                                style: const TextStyle(fontSize: 25),
+                            )
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              Share.share(
+                                  "The BMI Calculator said to me: < $bmiInterpretation > since my BMI score is ${bmiScore.toStringAsFixed(1)} at age $age");
+                            },
+                            child: const Text("Share",
+                              style: const TextStyle(fontSize: 25),
+                            )),
+                      ],
+                    )
+                  ]))),
+    );
+  }
+
+  void setBmiInterpretation() {
+    if (bmiScore > 30) {
+      bmiStatus = "Obese";
+      bmiInterpretation = "Please work to reduce obesity";
+      bmiStatusColor = Colors.pink;
+    } else if (bmiScore >= 25) {
+      bmiStatus = "Overweight";
+      bmiInterpretation = "Do regular exercise & reduce the weight";
+      bmiStatusColor = Colors.orange;
+    } else if (bmiScore >= 18.5) {
+      bmiStatus = "Normal";
+      bmiInterpretation = "Enjoy, You are fit";
+      bmiStatusColor = Colors.green;
+    } else if (bmiScore < 18.5) {
+      bmiStatus = "Underweight";
+      bmiInterpretation = "Try to increase the weight";
+      bmiStatusColor = Colors.red;
+    }
+  }
+}
